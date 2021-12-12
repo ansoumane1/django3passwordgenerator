bff7ccb3a3baa772ee73d5e7673d5c5386cf2c0a
Ansoumane <ansoumanesoumaoro07@gmail.com>
2021-12-12 15:11:30 +0100

    Our first commit!

---
+++ Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Ansoumane\Desktop\formation_django\GENERATORPASSWORD\Env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
